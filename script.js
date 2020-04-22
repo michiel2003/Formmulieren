507373b16bef9729c873b72b6a960a6855c79087
michiel2003 <michieldecap@gmail.com>
2020-04-22 12:06:45 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function test(){
+    if(document.getElementById("fname").value ==""){
+        window.alert("please give a first name");
+    }
+    if(document.getElementById("lname").value == ""){
+        window.alert("please give a last name");
+    }
+    
+    if(document.getElementById("email").value.includes("@") == "false"){
+
+    }
+}
